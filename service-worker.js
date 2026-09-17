1ec3671251b55c0854a45fd7224f722a00f788c4
XERO820 <cobrakaichanal@gmail.com>
2026-09-17 11:31:03 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'xero4484-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.svg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
